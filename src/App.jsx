75953df2b3bc8b04320ac454f2cbac7897da3fe6
Ankita Ahuja <ankitaahuja111@gmail.com>
2018-10-02 14:07:51 -0400

    set the initial state and passed it to currentUsers and Messages as prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import React, {Component} from 'react';
-import ChatBar from './ChatBar.jsx'
-import MessageList from './MessageList.jsx'
-import NavBar from './NavBar.jsx'
+import React, { Component } from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
+import NavBar from './NavBar.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // this is the *only* time you should assign directly to state:
+    this.state = {
+      currentUser: { name: 'Bob' }, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          id: 1,
+          username: 'Bob',
+          content: 'Has anyone seen my marbles?'
+        },
+        {
+          id: 2,
+          username: 'Anonymous',
+          content:
+            'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
+        }
+      ]
+    };
+  }
+
   render() {
     return (
-    <div>
-      <NavBar /> 
-      <MessageList />
-      <ChatBar />
-    
-    </div>
-    
+      <div>
+        <NavBar />
+        <MessageList messages={this.state.messages} />
+        <ChatBar currentUser={this.state.currentUser} />
+      </div>
     );
   }
 }
